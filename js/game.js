bb0c55c2ddaa5f95396e496affc7b9fb7321a627
StevenLe459 <steven.le740@my.tccd.edu>
2020-02-23 19:36:33 -0600

    Creating video game in website

---
+++ js/game.js
@@ -0,0 +1,58 @@
+// a key map of allowed keys
+var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b'
+};
+  
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+// get the value of the key code from the key map
+var key = allowedKeys[e.keyCode];
+// get the value of the required key from the konami code
+var requiredKey = konamiCode[konamiCodePosition];
+
+// compare the key with the required key
+if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
+    startGame();
+    konamiCodePosition = 0;
+    }
+} 
+
+else {
+    konamiCodePosition = 0;
+}
+});
+
+function startGame() {
+    document.body.innerHTML = "";
+    var newh1 = document.createElement("h1");
+    var newtxt = document.createTextNode("PRESS ENTER TO CONTINUE");
+    document.body.appendChild(newh1);
+    newh1.appendChild(newtxt);
+    newh1.className = "center-align video-game";
+    $(this).on('keypress', function(event) {
+        if (event.keyCode == 13) {
+            game();
+        }
+    })
+}
+
+function game() {
+    alert("Under construction!");
+}
